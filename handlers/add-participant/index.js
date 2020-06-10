3d20fd7a592fe48ee3637ec59c963afb9c35d77c
TMowka <deftassassin@gmail.com>
2020-06-10 22:57:40 +0300

    refactor: remove participants logic

--- handlers/add-participant/index.js
+++
@@ -1,5 +0,0 @@
-const participants = require('../../participants');
-
-module.exports = async function (chatId, message) {
-  participants.add(message);
-};
--- handlers/index.js
+++ handlers/index.js
@@ -1,11 +1,7 @@
 const helloHandler = require('./hello');
-const addParticipantHandler = require('./add-participant');
-const removeParticipantHandler = require('./remove-participant');
 const remindHandler = require('./remind');
 
 module.exports = {
   '/hello': helloHandler,
-  '/addparticipant': addParticipantHandler,
-  '/removeparticipant': removeParticipantHandler,
   '/remind': remindHandler,
 };
